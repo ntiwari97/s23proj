56358d8a8c87108ef7f222a9e70c17b45cbc4aa8
Nick Tiwari <ntiwari@andrew.cmu.edu>
2023-04-25 16:30:30 -0400

    working command line functionality

--- s23proj/command.py
+++ s23proj/command.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import sys
 import argparse
-from project import Works
+from s23proj import Works
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-rt", "--reftype", help="The type of reference you which to output. Options are 'bibtex' and 'ris'")
-parser.add_argument("-doi", help="The type of reference you which to output. Options are 'bibtex' and 'ris'")
+parser.add_argument("doi", help="The DOI of the paper")
 
 args = parser.parse_args()
+doi = args.doi
+work = Works(doi)
 
-if 
+if args.reftype == "bibtex":
+    print(work.bibtex)
+elif args.reftype == "ris":
+    print(work.ris)
+else:
+    raise RuntimeError("Support for the citation style which you requested does not exist.")
